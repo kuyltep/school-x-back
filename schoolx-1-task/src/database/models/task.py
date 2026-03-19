1b3f3abd067f9b00e60d2bbbf1413e97e5e756d0
kuyltep <vladpetlu91@gmail.com>
2026-03-19 15:05:16 +0300

    update task model and repository

--- schoolx-1-task/src/database/models/task.py
+++ schoolx-1-task/src/database/models/task.py
@@ -1,11 +1,16 @@
+from typing import TYPE_CHECKING
+
 from uuid import UUID
 
 from src.database.database import Base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.types import Uuid
 
 from ..sql_enums import TaskStatus
 
+if TYPE_CHECKING:
+  from .user import User
 
 class Task(Base):
   title: Mapped[str]
@@ -13,3 +18,6 @@ class Task(Base):
   description: Mapped[str | None]
   asset_id: Mapped[UUID | None] = mapped_column(Uuid, index=True)
   version: Mapped[int] = mapped_column(default=1)
+  user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"), index=True)
+
+  user: Mapped["User"] = relationship("User", back_populates="tasks")
--- schoolx-1-task/src/database/repositories/task.py
+++ schoolx-1-task/src/database/repositories/task.py
@@ -1,7 +1,32 @@
 from src.database.repositories.base import BaseRepository
 from src.database.models.task import Task
-from src.database.sql_enums import TaskStatus
+from src.database.database import async_session_maker
+from typing import Any, List, Literal, Tuple
+from uuid import UUID
+
+from sqlalchemy import select
 
 
 class TaskRepository(BaseRepository):
   model = Task
+
+  @classmethod
+  async def get_by_user_id(
+    cls,
+    user_id: UUID,
+    page: int = 1,
+    size: int = 10,
+    sort_by: str = "created_at",
+    order: Literal["asc", "desc"] = "desc",
+  ) -> Tuple[List[Any], int]:
+    async with async_session_maker() as session:
+      query = select(cls.model).where(cls.model.user_id == user_id)
+
+      total = await cls._get_total_count(session, query)
+
+      query = cls._apply_sorting(query, sort_by, order)
+      query = query.offset((page - 1) * size).limit(size)
+
+      result = await session.execute(query)
+      items = result.scalars().all()
+      return items, total
