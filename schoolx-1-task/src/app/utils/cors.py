46524b4f1825d27dc8cc9f70edbd8ff133db4b2f
kuyltep <vladpetlu91@gmail.com>
2026-03-30 22:54:02 +0300

    add cors logic setup

--- schoolx-1-task/src/app/utils/cors.py
+++ schoolx-1-task/src/app/utils/cors.py
@@ -4,6 +4,21 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.app.config import configs
 
 
-def setup_cors(app: FastAPI):
+def setup_cors(app: FastAPI) -> None:
+  if configs.app_env.lower() == "dev":
+    app.add_middleware(
+      CORSMiddleware,
+      allow_origins=["*"],
+      allow_credentials=True,
+      allow_methods=["*"],
+      allow_headers=["*"],
+    )
+    return
 
-  CORSMiddleware(app=app, allow_methods="*", allow_origins=configs.origins)
+  app.add_middleware(
+    CORSMiddleware,
+    allow_origins=configs.origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+  )
